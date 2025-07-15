5a2eae9bc3590f0e61616dd4038a776900a7bfbd
Mateo Tascon Gomez <tascon1010100@gmail.com>
2025-07-15 22:04:27 +0200

    Register is almost ready, missing to implement useFetch, and do it the same to Login

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -2,14 +2,45 @@ import stylesRegister from "./Register.module.css";
 import stylesForm from "./Form.module.css";
 import { NavLink } from "react-router-dom";
 import { Title } from "../title";
+import { useState } from "react";
+
+interface RegisterData {
+  userName: string;
+  email: string;
+  passwd: string;
+}
 
 const Register = () => {
+  const [registerData, setRegisterData] = useState<RegisterData>({
+    userName: "",
+    email: "",
+    passwd: "",
+  });
+
+  const [errorForm, setErrorForm] = useState<string | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setRegisterData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (registerData.userName && registerData.email && registerData.passwd) {
+      console.log("Todo correcto");
+      setErrorForm(null);
+    } else {
+      setErrorForm("All fields are required");
+    }
+  };
+
   return (
     <div className={stylesForm.form}>
       <Title />
       <div className={stylesRegister.register}>
         <h2 className={stylesRegister.registerH2}>Register</h2>
-        <form className={stylesRegister.registerForm}>
+        <form className={stylesRegister.registerForm} onSubmit={handleSubmit}>
           <label
             htmlFor="userName"
             className={stylesRegister.registerFormLabel}
@@ -22,37 +53,37 @@ const Register = () => {
             id="userName"
             name="userName"
             placeholder="User Name"
+            onChange={handleChange}
           />
-          <label
-            htmlFor="userEmail"
-            className={stylesRegister.registerFormLabel}
-          >
+          <label htmlFor="email" className={stylesRegister.registerFormLabel}>
             Email
           </label>
           <input
             className={stylesRegister.registerFormInput}
             type="email"
-            id="userEmail"
-            name="userEmail"
+            id="email"
+            name="email"
             placeholder="Email"
+            onChange={handleChange}
           />
-          <label
-            htmlFor="userPassword"
-            className={stylesRegister.registerFormLabel}
-          >
+          <label htmlFor="passwd" className={stylesRegister.registerFormLabel}>
             Password
           </label>
           <input
             className={stylesRegister.registerFormInput}
             type="password"
-            id="userPassword"
-            name="userPassword"
+            id="passwd"
+            name="passwd"
             placeholder="Password"
+            onChange={handleChange}
           />
-          <button className={stylesRegister.registerFormButton}>
+          <button type="submit" className={stylesRegister.registerFormButton}>
             Register
           </button>
         </form>
+        {errorForm && (
+          <p className={stylesRegister.registerError}>{errorForm}</p>
+        )}
         <NavLink to="/" className={stylesRegister.registerNavLink}>
           If you are registered access to TheWall
         </NavLink>
